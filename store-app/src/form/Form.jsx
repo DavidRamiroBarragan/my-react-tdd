eb8014a45269b7cdceefa5eda1f33dd91a7a006d
M. David Ramiro <dramirobarragan@gmail.com>
2022-01-19 21:23:24 +0100

    files formatted

--- store-app/src/form/Form.jsx
+++ store-app/src/form/Form.jsx
@@ -1,54 +1,72 @@
-import {Button, InputLabel, Select, TextField} from '@material-ui/core';
-import {useState} from 'react';
+import { Button, InputLabel, Select, TextField } from '@material-ui/core'
+import { useState } from 'react'
 
 export const Form = () => {
-    const [formErrors, setFormErrors] = useState({name: '', size: '', type: ''});
-    const handleSubmit                = (event) => {
-        event.preventDefault();
-        const {name, size, type} = event.target.elements;
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    size: '',
+    type: '',
+  })
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { name, size, type } = event.target.elements
 
-        if (!name.value) {
-            setFormErrors((prev) => ({...prev, name: 'The name is required'}));
-        }
-        if (!size.value) {
-            setFormErrors((prev) => ({...prev, size: 'The size is required'}));
-        }
-        if (!type.value) {
-            setFormErrors((prev) => ({...prev, type: 'The type is required'}));
-        }
-    };
-
-    const handleBlur = (event) => {
-        const {name, value} = event.target;
-        setFormErrors({...formErrors, [name]: value.length ? '' : `The ${name} is required`})
+    if (!name.value) {
+      setFormErrors((prev) => ({ ...prev, name: 'The name is required' }))
     }
-    return (
-        <>
-            <h1>Create product</h1>
-            <form onSubmit={handleSubmit}>
-                <TextField label={'name'} id="name" helperText={formErrors.name} onBlur={handleBlur} name="name"/>
-                <TextField label={'size'} id="size" helperText={formErrors.size} name="size" onBlur={handleBlur}/>
-                    <InputLabel variant="standard" htmlFor="type">
-                        Type
-                    </InputLabel>
-                    <Select
-                        native
-                        value=""
-                        inputProps={{
-                            name: 'type',
-                            id: 'type',
-                        }}
-                        onBlur={handleBlur}
+    if (!size.value) {
+      setFormErrors((prev) => ({ ...prev, size: 'The size is required' }))
+    }
+    if (!type.value) {
+      setFormErrors((prev) => ({ ...prev, type: 'The type is required' }))
+    }
+  }
 
-                    >
-                        <option aria-label="None" value="" />
-                        <option value="electronic">Electronic</option>
-                        <option value="furniture">Furniture</option>
-                        <option value="clothing">Clothing</option>
-                    </Select>
-                    {formErrors.type.length &&<p>{formErrors.type}</p>}
-                <Button type="submit">Submit</Button>
-            </form>
-        </>
-    );
-};
+  const handleBlur = (event) => {
+    const { name, value } = event.target
+    setFormErrors({
+      ...formErrors,
+      [name]: value.length ? '' : `The ${name} is required`,
+    })
+  }
+  return (
+    <>
+      <h1>Create product</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="name"
+          id="name"
+          helperText={formErrors.name}
+          onBlur={handleBlur}
+          name="name"
+        />
+        <TextField
+          label="size"
+          id="size"
+          helperText={formErrors.size}
+          name="size"
+          onBlur={handleBlur}
+        />
+        <InputLabel variant="standard" htmlFor="type">
+          Type
+        </InputLabel>
+        <Select
+          native
+          value=""
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}
+          onBlur={handleBlur}
+        >
+          <option aria-label="None" value=""/>
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </Select>
+        {formErrors.type.length && <p>{formErrors.type}</p>}
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
+  )
+}
--- store-app/src/form/Form.test.jsx
+++ store-app/src/form/Form.test.jsx
@@ -1,69 +1,68 @@
-import userEvent from '@testing-library/user-event';
-import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
-import {Form} from './Form';
+import userEvent from "@testing-library/user-event";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Form } from "./Form";
 
-    beforeEach(() => {render(<Form/>)})
-describe('when the component is mounted', () => {
-    it('should be a create product form page.', function() {
-        expect(screen.getByRole('heading', {name: /create product/i})).toBeInTheDocument();
-    });
-    it('should exist the fields: name, size, type (electronic, furniture, clothing)',
-       function() {
-            expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
-            expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
-            expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
-            expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
-       });
-    it('should exists the submit button', () => {
-        expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument()
-    });
-
+beforeEach(() => {
+  render(<Form />);
 });
 
-describe('when the use submits the form without values', () => {
-    it('should display validation messages', () => {
-        expect(screen.queryByText(/The name is required/i)).not.toBeInTheDocument()
+describe("when the component is mounted", () => {
+  it("should be a create product form page.", function () {
+    expect(
+      screen.getByRole("heading", { name: /create product/i })
+    ).toBeInTheDocument();
+  });
+  it("should exist the fields: name, size, type (electronic, furniture, clothing)", function () {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument();
+  });
+  it("should exists the submit button", () => {
+    expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
+  });
+});
 
-        userEvent.click(screen.getByRole('button', {name: /submit/i}))
+describe("when the use submits the form without values", () => {
+  it("should display validation messages", () => {
+    expect(screen.queryByText(/The name is required/i)).not.toBeInTheDocument();
 
-        expect(screen.queryByText(/The name is required/i)).toBeInTheDocument()
-        expect(screen.queryByText(/The size is required/i)).toBeInTheDocument()
-        expect(screen.queryByText(/The type is required/i)).toBeInTheDocument()
-    })
-})
+    userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+    expect(screen.queryByText(/The name is required/i)).toBeInTheDocument();
+    expect(screen.queryByText(/The size is required/i)).toBeInTheDocument();
+    expect(screen.queryByText(/The type is required/i)).toBeInTheDocument();
+  });
+});
 
 describe("when the user blurs an empty field", () => {
-    it('should display validation message for the input name', () => {
+  it("should display validation message for the input name", () => {
+    expect(screen.queryByText(/The name is required/i)).not.toBeInTheDocument();
 
-        expect(screen.queryByText(/The name is required/i)).not.toBeInTheDocument();
+    fireEvent.blur(screen.getByLabelText(/name/i), {
+      target: { name: "name", value: "" },
+    });
 
-        fireEvent.blur(screen.getByLabelText(/name/i), {
-            target:{ name: 'name', value: ''}
-        });
+    expect(screen.queryByText(/The name is required/i)).toBeInTheDocument();
+  });
 
-        expect(screen.queryByText(/The name is required/i)).toBeInTheDocument()
-    })
+  it("should display validation message for the input size", () => {
+    expect(screen.queryByText(/The size is required/i)).not.toBeInTheDocument();
 
-    it('should display validation message for the input size', () => {
+    fireEvent.blur(screen.getByLabelText(/size/i), {
+      target: { name: "size", value: "" },
+    });
 
-        expect(screen.queryByText(/The size is required/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/The size is required/i)).toBeInTheDocument();
+  });
 
-        fireEvent.blur(screen.getByLabelText(/size/i), {
-            target:{ name: 'size', value: ''}
-        });
+  it("should display validation message for the input type", () => {
+    expect(screen.queryByText(/The type is required/i)).not.toBeInTheDocument();
 
-        expect(screen.queryByText(/The size is required/i)).toBeInTheDocument()
-    })
+    fireEvent.blur(screen.getByLabelText(/type/i), {
+      target: { name: "type", value: "" },
+    });
 
-    it('should display validation message for the input type', () => {
-
-        expect(screen.queryByText(/The type is required/i)).not.toBeInTheDocument()
-
-        fireEvent.blur(screen.getByLabelText(/type/i), {
-            target:{ name: 'type', value: ''}
-        });
-
-        expect(screen.queryByText(/The type is required/i)).toBeInTheDocument()
-    })
-})
+    expect(screen.queryByText(/The type is required/i)).toBeInTheDocument();
+  });
+});
