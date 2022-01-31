cdcd1d861df37f4b8dfa61b543fd68cfb5129df2
David Ramiro <dramirobarragan@gmail.com>
2022-01-31 20:13:49 +0100

    test table cells

--- github-search/src/components/github-serach-page/github-serach-page.js
+++ github-search/src/components/github-serach-page/github-serach-page.js
@@ -1,51 +1,63 @@
-import {Box, Container, Grid} from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import {useState} from 'react';
+import { Box, Container, Grid } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import { useState } from 'react'
 
 export const GitHubSearchPage = () => {
-    const [isSearching, setIsSearching] = useState(false);
-    const [isSearchApplied, setIsSearchApplied] = useState(false)
+  const [isSearching, setIsSearching]         = useState(false)
+  const [isSearchApplied, setIsSearchApplied] = useState(false)
 
-    const handleSearch = async () => {
-        setIsSearching(() => true);
-        await Promise.resolve()
-        setIsSearchApplied(true);
-        setIsSearching(() => false);
-    };
+  const handleSearch = async () => {
+    setIsSearching(() => true)
+    await Promise.resolve()
+    setIsSearchApplied(true)
+    setIsSearching(() => false)
+  }
 
-    const renderContentTable = () => {
-        return <table>
-            <thead>
-            <tr>
-                <th>Repository</th>
-                <th>stars</th>
-                <th>forks</th>
-                <th>open issues</th>
-                <th>updated at</th>
-            </tr>
-            </thead>
-        </table>
-    }
+  const renderContentTable = () => {
+    return <table>
+      <thead>
+      <tr>
+        <th>
+          Repository
+        </th>
+        <th>stars</th>
+        <th>forks</th>
+        <th>open issues</th>
+        <th>updated at</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td><img src="" alt="test"/>
+          <a href="http://localhost:3000">Test</a></td>
+        <td>10</td>
+        <td>5</td>
+        <td>2</td>
+        <td>20-01-01</td>
+      </tr>
+      </tbody>
+    </table>
+  }
 
-    return <Container>
-        <Typography variant="h3" component="h1">Github repositories list</Typography>
-        <Grid container spacing={2} justifyContent="space-between">
-            <Grid item md={6} xs={12}>
-                <TextField fullWidth label="Filter by" id="filterBy"/>
-            </Grid>
-            <Grid item md={3} xs={12}>
-                <Button fullWidth variant="contained" color="primary" onClick={handleSearch}
-                        disabled={isSearching}>Search</Button>
-            </Grid>
-        </Grid>
-        {isSearchApplied && renderContentTable() }
-        {!isSearchApplied &&
-        <Box display="flex" alignItems={'center'} justifyContent="center" height={400}>
-            <Typography>Please provide a search option and click in the search button</Typography>
-        </Box>}
-    </Container>;
-};
+  return <Container>
+    <Typography variant="h3" component="h1">Github repositories list</Typography>
+    <Grid container spacing={2} justifyContent="space-between">
+      <Grid item md={6} xs={12}>
+        <TextField fullWidth label="Filter by" id="filterBy"/>
+      </Grid>
+      <Grid item md={3} xs={12}>
+        <Button fullWidth variant="contained" color="primary" onClick={handleSearch}
+                disabled={isSearching}>Search</Button>
+      </Grid>
+    </Grid>
+    {isSearchApplied && renderContentTable()}
+    {!isSearchApplied &&
+      <Box display="flex" alignItems={'center'} justifyContent="center" height={400}>
+        <Typography>Please provide a search option and click in the search button</Typography>
+      </Box>}
+  </Container>
+}
 
-export default GitHubSearchPage;
+export default GitHubSearchPage
--- github-search/src/components/github-serach-page/github-serach-page.test.js
+++ github-search/src/components/github-serach-page/github-serach-page.test.js
@@ -1,68 +1,89 @@
-import {fireEvent, render, screen, waitFor, within} from '@testing-library/react';
-import {GitHubSearchPage} from './github-serach-page';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react'
+import { GitHubSearchPage } from './github-serach-page'
 
-let searchButton;
+let searchButton
 beforeEach(() => {
-    // eslint-disable-next-line testing-library/no-render-in-setup
-    render(<GitHubSearchPage/>);
-    searchButton = screen.getByRole('button', {name: /search/i});
-});
-describe('whn the GithubSearchPage is mounted', function() {
-    it('should display the title', function() {
-        expect(screen.getByRole('heading', {name: /github repositories list/i})).toBeInTheDocument();
-    });
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  render(<GitHubSearchPage/>)
+  searchButton = screen.getByRole('button', { name: /search/i })
+})
+describe('whn the GithubSearchPage is mounted', function () {
+  it('should display the title', function () {
+    expect(screen.getByRole('heading', { name: /github repositories list/i })).toBeInTheDocument()
+  })
 
-    it('should contain an input text with label "filter by" field in order to do the search', () => {
+  it('should contain an input text with label "filter by" field in order to do the search', () => {
 
-        expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument();
-    });
+    expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument()
+  })
 
-    it('should contain a search button', () => {
-        expect(screen.getByRole('button', {name: /search/i})).toBeInTheDocument();
-    });
+  it('should contain a search button', () => {
+    expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument()
+  })
 
-    it('should be a initial message \'Please provide a search option and click in the search button\'', () => {
-        expect(screen.getByText(/please provide a search option and click in the search button/i)).toBeInTheDocument();
-    });
-});
+  it('should be a initial message \'Please provide a search option and click in the search button\'', () => {
+    expect(screen.getByText(/please provide a search option and click in the search button/i)).toBeInTheDocument()
+  })
+})
 
 describe('when the developers does a search', () => {
-    const fireClickSearch = () => fireEvent.click(searchButton);
-    it('the search button should be disabled until the search is done', async () => {
-        expect(searchButton).not.toBeDisabled();
+  const fireClickSearch = () => fireEvent.click(searchButton)
+  it('the search button should be disabled until the search is done', async () => {
+    expect(searchButton).not.toBeDisabled()
 
-        fireClickSearch();
+    fireClickSearch()
 
-        expect(searchButton).toBeDisabled();
+    expect(searchButton).toBeDisabled()
 
-        await waitFor(() => {
-            expect(searchButton).not.toBeDisabled();
-        });
+    await waitFor(() => {
+      expect(searchButton).not.toBeDisabled()
+    })
 
-    });
-    it('The data should be displayed as a sticky table', async () => {
-        fireClickSearch();
-        await waitFor(() => {
-            expect(screen.queryByText(/please provide a search option and click in the search button/i))
-                .not
-                .toBeInTheDocument();
-        });
+  })
+  it('The data should be displayed as a sticky table', async () => {
+    fireClickSearch()
+    await waitFor(() => {
+      expect(screen.queryByText(/please provide a search option and click in the search button/i))
+        .not
+        .toBeInTheDocument()
+    })
 
-        expect(screen.getByRole('table')).toBeInTheDocument();
-    });
+    expect(screen.getByRole('table')).toBeInTheDocument()
+  })
 
-    it('The header table should contain: Repository, stars, forks, open issues and updated at', async () => {
-        fireClickSearch();
+  it('The header table should contain: Repository, stars, forks, open issues and updated at', async () => {
+    fireClickSearch()
 
-        const table                                             = await screen.findByRole('table');
-        const tableHeaders                                      = within(table).getAllByRole('columnheader');
-        const [repository, stars, forks, openIssues, updatedAt] = tableHeaders;
+    const table        = await screen.findByRole('table')
+    const withTable    = within(table)
+    const tableHeaders = withTable.getAllByRole('columnheader')
 
-        expect(tableHeaders).toHaveLength(5);
-        expect(repository).toHaveTextContent(/repository/i);
-        expect(stars).toHaveTextContent(/stars/i);
-        expect(forks).toHaveTextContent(/forks/i);
-        expect(openIssues).toHaveTextContent(/open issues/i);
-        expect(updatedAt).toHaveTextContent(/updated at/i);
-    });
-});
+    expect(withTable.getByRole('img', { name: /test/i })).toBeInTheDocument()
+    const [repository, stars, forks, openIssues, updatedAt] = tableHeaders
+
+    expect(tableHeaders).toHaveLength(5)
+    expect(repository).toHaveTextContent(/repository/i)
+    expect(stars).toHaveTextContent(/stars/i)
+    expect(forks).toHaveTextContent(/forks/i)
+    expect(openIssues).toHaveTextContent(/open issues/i)
+    expect(updatedAt).toHaveTextContent(/updated at/i)
+    expect(withTable.getByText(/test/i).closest('a')).toHaveAttribute('href', 'http://localhost:3000')
+  })
+
+  it('each table result must contain: owner avatar image, stars, updated at, forks, open issues', async () => {
+    fireClickSearch()
+
+    const table                                             = await screen.findByRole('table')
+    const withTable                                         = within(table)
+    const tableCells                                        = withTable.getAllByRole('cell')
+    const [repository, stars, forks, openIssues, updatedAt] = tableCells
+
+    expect(repository).toHaveTextContent(/test/i)
+    expect(stars).toHaveTextContent(/10/i)
+    expect(forks).toHaveTextContent(/5/i)
+    expect(openIssues).toHaveTextContent(/2/i)
+    expect(updatedAt).toHaveTextContent(/20-01-01/i)
+    expect(within(tableCells[0]).getByText(/test/i).closest('a')).toHaveAttribute('href', 'http://localhost:3000')
+
+  })
+})
